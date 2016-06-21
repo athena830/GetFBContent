f3feb36e0f870557139833e5d16d138b2bf43699
athena <athena@gis.tw>
2016-06-21 17:42:52 +0800

    增加try{}catch{}處理會有無法找到頁面高度的問題(但無效)

--- Form1.cs
+++ Form1.cs
@@ -98,69 +98,81 @@ namespace GetFBContent
             System.Windows.Forms.HtmlDocument document = this.webBrowser1.Document;
             document.Window.ScrollTo(0, 1000000000);
             FBService.getFilterKey();
-            if (rt == document.Window.Size.Height)
-            {
-                timer1.Stop();
-                try
+            //if (document.Window.WindowFrameElement != null)
+            //{
+                if (rt == document.Window.Size.Height)
                 {
-                    System.IO.StreamReader getReader = new System.IO.StreamReader(this.webBrowser1.DocumentStream, System.Text.Encoding.Default);
-                    string HTML = getReader.ReadToEnd();
-                    string Message_Condition_start = "<DIV class=\"_5pbx userContent\"";
-                    string Message_Condition_finish = "</P>";
-                    string Paragraph_start = "<P>";
-                    string time_start = "data-utime=\"";
-                    string time_finish = "\"";
-                    int time_Length = 0;
-                    int dfg = 0;
-                    int KeywordID = -1; //關鍵字ID
-                    //從0開始搜尋time_start
-                    while (HTML.IndexOf(time_start, time_Length) != -1)
+                    timer1.Stop();
+                    try
                     {
-                        int timeStart = HTML.IndexOf(time_start, time_Length);
-
-                        //從0開始搜尋Message_Condition_start
-                        //int htmlStart = HTML.IndexOf(Message_Condition_start, time_Length);
-                        //從0開始搜尋time_start位置再找time_finish的位置
-                        //int htmlend = HTML.IndexOf(time_finish, HTML.IndexOf(time_start, time_Length));
-
-                        //換個想法，從time_start位置找Message_Condition_start
-                        int htmlStart = HTML.IndexOf(Message_Condition_start, time_Length);
-                        //從Message_Condition_start去找Message_Condition_finish
-                        if (htmlStart != -1)
+                        System.IO.StreamReader getReader = new System.IO.StreamReader(this.webBrowser1.DocumentStream, System.Text.Encoding.Default);
+                        string HTML = getReader.ReadToEnd();
+                        string Message_Condition_start = "<DIV class=\"_5pbx userContent\"";
+                        string Message_Condition_finish = "</P>";
+                        string Paragraph_start = "<P>";
+                        string time_start = "data-utime=\"";
+                        string time_finish = "\"";
+                        int time_Length = 0;
+                        int dfg = 0;
+                        int KeywordID = -1; //關鍵字ID
+                        //從0開始搜尋time_start
+                        while (HTML.IndexOf(time_start, time_Length) != -1)
                         {
-                            int htmlend = HTML.IndexOf(Message_Condition_finish, htmlStart);
-                            if (htmlStart < htmlend)
+                            int timeStart = HTML.IndexOf(time_start, time_Length);
+
+                            //從0開始搜尋Message_Condition_start
+                            //int htmlStart = HTML.IndexOf(Message_Condition_start, time_Length);
+                            //從0開始搜尋time_start位置再找time_finish的位置
+                            //int htmlend = HTML.IndexOf(time_finish, HTML.IndexOf(time_start, time_Length));
+
+                            //換個想法，從time_start位置找Message_Condition_start
+                            int htmlStart = HTML.IndexOf(Message_Condition_start, time_Length);
+                            //從Message_Condition_start去找Message_Condition_finish
+                            if (htmlStart != -1)
                             {
-                                DateTime time = StampToDateTime(HTML.Substring(timeStart + time_start.Length, HTML.IndexOf(time_finish, timeStart) - time_finish.Length - timeStart));
-                                string Message = HTML.Substring(htmlStart + Message_Condition_start.Length, HTML.IndexOf(Message_Condition_finish, htmlStart) - Message_Condition_finish.Length - htmlStart);
-                                string msg = Message.Substring(Message.IndexOf(Paragraph_start) + 3, Message.IndexOf(Message_Condition_finish) - Message.IndexOf(Paragraph_start) - 3);
+                                int htmlend = HTML.IndexOf(Message_Condition_finish, htmlStart);
+                                if (htmlStart < htmlend)
+                                {
+                                    DateTime time = StampToDateTime(HTML.Substring(timeStart + time_start.Length, HTML.IndexOf(time_finish, timeStart) - time_finish.Length - timeStart));
+                                    string Message = HTML.Substring(htmlStart + Message_Condition_start.Length, HTML.IndexOf(Message_Condition_finish, htmlStart) - Message_Condition_finish.Length - htmlStart);
+                                    string msg = Message.Substring(Message.IndexOf(Paragraph_start) + 3, Message.IndexOf(Message_Condition_finish) - Message.IndexOf(Paragraph_start) - 3);
 
-                                KeywordID = FBService.matchTitle(msg);
-                                FBService.insertFB(dfg, place_id, msg, time, KeywordID);
-                                dfg++;
+                                    KeywordID = FBService.matchTitle(msg);
+                                    FBService.insertFB(dfg, place_id, msg, time, KeywordID);
+                                    dfg++;
+                                }
+                                time_Length = htmlend;
                             }
-                            time_Length = htmlend;
+                            else
+                            {
+                                break;
+                            }
                         }
-                        else
-                        {
-                            break;
-                        }
+                        FBService.updFBplace(1, place_id);
                     }
-                    FBService.updFBplace(1, place_id);
+                    catch (Exception ex)
+                    {
+                        FBService.updFBplace(-1, place_id);
+                    }
+                    DataTable dt = FBService.getFBNewID();
+                    place_id = dt.Rows[0]["id"].ToString();
+                    webBrowser1.Navigate("https://www.facebook.com/pages/" + dt.Rows[0]["name"].ToString() + "/" + place_id);
+                    timer1.Start();
                 }
-                catch (Exception ex)
+                else
                 {
-                    FBService.updFBplace(-1, place_id);
+                    rt = document.Window.Size.Height;
                 }
-                DataTable dt = FBService.getFBNewID();
-                place_id = dt.Rows[0]["id"].ToString();
-                webBrowser1.Navigate("https://www.facebook.com/pages/" + dt.Rows[0]["name"].ToString() + "/" + place_id);
-                timer1.Start();
-            }
-            else
-            {
-                rt = document.Window.Size.Height;
-            }
+            //}
+            //else
+            //{
+            //    FBService.updFBplace(-1, place_id);
+            //    DataTable dt = FBService.getFBNewID();
+            //    place_id = dt.Rows[0]["id"].ToString();
+            //    webBrowser1.Navigate("https://www.facebook.com/pages/" + dt.Rows[0]["name"].ToString() + "/" + place_id);
+            //    timer1.Start();
+            //}
+
         }
     }
 }
